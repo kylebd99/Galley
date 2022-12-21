3db6de714d12f65371bf3d2ba3fddefdf48d9d6c
kylebd99 <kylebd99@gmail.com>
2022-12-21 11:08:56 -0800

    Initializing the Repository

---
+++ Source/ExtendedFreeJoinPlan.jl
@@ -0,0 +1,6 @@
+
+# This file defines the query plan struct which stores the physical execution plan for a particular query.
+# In this system, query plans are an extension of FreeJoin plans (include link to Remy's paper here) which 
+# additionally handle aggregation. 
+#
+#

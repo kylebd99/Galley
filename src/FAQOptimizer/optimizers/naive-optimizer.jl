079859f7d173eb7e205bf261237ce081c4d79f15
kylebd99 <kdeeds@cs.washington.edu>
2024-02-29 17:16:49 -0800

    add stats type annotation

--- src/FAQOptimizer/optimizers/naive-optimizer.jl
+++ src/FAQOptimizer/optimizers/naive-optimizer.jl
@@ -7,7 +7,7 @@ function naive_decomposition(faq::FAQInstance)
     sum_op = faq.sum_op
     output_indices = faq.output_indices
     output_index_order = faq.output_index_order
-    factors = faq.factors
+    factors = Set{Factor{ST}}(faq.factors)
     bag::Bag{ST} = Bag(mult_op, sum_op, factors, faq.input_indices, faq.output_indices, Set{Bag{ST}}(), 0)
     return HyperTreeDecomposition(mult_op, sum_op, output_indices, bag, output_index_order)
 end

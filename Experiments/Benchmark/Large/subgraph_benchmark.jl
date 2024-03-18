9f9b7c966dbade4b96b1047c5c15fec13107b1a1
kylebd99 <kdeeds@cs.washington.edu>
2024-03-18 12:08:50 -0700

    add compile time graph

--- Experiments/Benchmark/Large/subgraph_benchmark.jl
+++ Experiments/Benchmark/Large/subgraph_benchmark.jl
@@ -2,7 +2,7 @@ include("../../Experiments.jl")
 
 
 #datasets = instances(WORKLOAD)
-datasets = [aids]
+datasets = [hprd_lite]
 
 experiments = ExperimentParams[]
 for dataset in datasets
@@ -10,13 +10,14 @@ for dataset in datasets
 #    push!(experiments, ExperimentParams(workload=dataset, faq_optimizer=hypertree_width))
     push!(experiments, ExperimentParams(workload=dataset, faq_optimizer=greedy; stats_type=DCStats, warm_start=true, description="Ours", timeout=500))
 #    push!(experiments, ExperimentParams(workload=dataset, faq_optimizer=greedy; stats_type=DCStats, use_duckdb=true, description="Ours + DuckDB", timeout=500))
-#    push!(experiments, ExperimentParams(workload=dataset, faq_optimizer=naive; stats_type=NaiveStats, use_duckdb=true, description="DuckDB", timeout=500))
+    push!(experiments, ExperimentParams(workload=dataset, faq_optimizer=naive; stats_type=NaiveStats, use_duckdb=true, description="DuckDB", timeout=500))
 #    push!(experiments, ExperimentParams(workload=dataset, faq_optimizer=greedy; stats_type=NaiveStats))
 #    push!(experiments, ExperimentParams(workload=dataset, faq_optimizer=ordering))
 end
 
 run_experiments(experiments)
 
-graph_grouped_box_plot(experiments; y_type=overall_time, grouping=description, filename="aids_subgraph_counting_overall")
-graph_grouped_box_plot(experiments; y_type=opt_time, grouping=description, filename="aids_subgraph_counting_opt")
-graph_grouped_box_plot(experiments; y_type=execute_time, grouping=description, filename="aids_subgraph_counting_execute")
+graph_grouped_box_plot(experiments; y_type=overall_time, grouping=description, filename="hprd_lite_subgraph_counting_overall")
+graph_grouped_box_plot(experiments; y_type=opt_time, grouping=description, filename="hprd_lite_subgraph_counting_opt")
+graph_grouped_box_plot(experiments; y_type=execute_time, grouping=description, filename="hprd_lite_subgraph_counting_execute")
+graph_grouped_box_plot(experiments; y_type=compile_time, grouping=description, filename="hprd_lite_subgraph_counting_compile")

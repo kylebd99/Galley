733e099115766195014d515df396f47d9f2e3681
kylebd99 <kdeeds@cs.washington.edu>
2024-05-10 13:05:57 -0700

    update for new query format

--- Experiments/Benchmark/Small/yeast-small.jl
+++ Experiments/Benchmark/Small/yeast-small.jl
@@ -6,62 +6,32 @@ using Galley: initmax, _calc_dc_from_structure, IndexExpr
 include("../../Experiments.jl")
 
 function query_triangle(e1, e2, e3)
-    i = IndexExpr("i")
-    j = IndexExpr("j")
-    k = IndexExpr("k")
-    e1 = e1[i,j]
-    e2 = e2[k,j]
-    e3 = e3[i,k]
-    factors = Set(Factor[Factor(e1, Set(IndexExpr[i, j]), Set(IndexExpr[i, j]), false, deepcopy(e1.stats), 1),
-                     Factor(e2, Set(IndexExpr[j, k]), Set(IndexExpr[j, k]), false, deepcopy(e2.stats), 2),
-                     Factor(e3, Set(IndexExpr[k, i]), Set(IndexExpr[k, i]), false, deepcopy(e3.stats), 3),
-    ])
-    faq = FAQInstance(*, +, Set{IndexExpr}(), Set{IndexExpr}([i, j, k]), factors)
-    return faq
+    e1 = relabel_input(e1, :i, :j)
+    e2 = relabel_input(e2, :k, :j)
+    e3 = relabel_input(e3, :i, :k)
+    query = Query(:out, Materialize(Aggregate(+, :i, :j, :k, MapJoin(*, e1, e2, e3))))
+    return query
 end
 
-function query_path(e1, e2, e3, e4)
-    i = IndexExpr("i")
-    j = IndexExpr("j")
-    k = IndexExpr("k")
-    l = IndexExpr("l")
-    m = IndexExpr("m")
-    e1 = e1[i,j]
-    e2 = e2[j,k]
-    e3 = e3[k,l]
-    e4 = e4[l,m]
-    factors = Set(Factor[Factor(e1, Set(IndexExpr[i, j]), Set(IndexExpr[i, j]), false, deepcopy(e1.stats), 1),
-                     Factor(e2, Set(IndexExpr[k, j]), Set(IndexExpr[j, k]), false, deepcopy(e2.stats), 2),
-                     Factor(e3, Set(IndexExpr[l, k]), Set(IndexExpr[k, l]), false, deepcopy(e3.stats), 3),
-                     Factor(e4, Set(IndexExpr[m, l]), Set(IndexExpr[l, m]), false, deepcopy(e4.stats), 4),
-    ])
 
-    faq = FAQInstance(*, +, Set{IndexExpr}(), Set{IndexExpr}([i, j, k, l, m]), factors)
-    return faq
+function query_path(e1, e2, e3, e4)
+    e1 = relabel_input(e1, :i, :j)
+    e2 = relabel_input(e2, :j, :k)
+    e3 = relabel_input(e3, :k, :l)
+    e4 = relabel_input(e4, :l, :m)
+    query = Query(:out, Materialize(Aggregate(+, :i, :j, :k, :l, :m, MapJoin(*, e1, e2, e3, e4))))
+    return query
 end
 
 function query_bowtie(e1, e2, e3, e4, e5, e6)
-    i = IndexExpr("i")
-    j = IndexExpr("j")
-    k = IndexExpr("k")
-    l = IndexExpr("l")
-    m = IndexExpr("m")
-    e1 = e1[i,j]
-    e2 = e2[j,k]
-    e3 = e3[i,k]
-    e4 = e4[l,m]
-    e5 = e5[l,k]
-    e6 = e6[m,k]
-    factors = Set(Factor[Factor(e1, Set(IndexExpr[i, j]), Set(IndexExpr[i, j]), false, deepcopy(e1.stats), 1),
-                     Factor(e2, Set(IndexExpr[j, k]), Set(IndexExpr[j, k]), false, deepcopy(e2.stats), 2),
-                     Factor(e3, Set(IndexExpr[i, k]), Set(IndexExpr[i, k]), false, deepcopy(e3.stats), 3),
-                     Factor(e4, Set(IndexExpr[l, m]), Set(IndexExpr[l, m]), false, deepcopy(e4.stats), 4),
-                     Factor(e5, Set(IndexExpr[l, k]), Set(IndexExpr[l, k]), false, deepcopy(e5.stats), 5),
-                     Factor(e6, Set(IndexExpr[m, k]), Set(IndexExpr[m, k]), false, deepcopy(e6.stats), 6),
-    ])
-
-    faq = FAQInstance(*, +, Set{IndexExpr}(), Set{IndexExpr}([i, j, k, l, m]), factors)
-    return faq
+    e1 = relabel_input(e1, :i, :j)
+    e2 = relabel_input(e2, :j, :k)
+    e3 = relabel_input(e3, :i, :k)
+    e4 = relabel_input(e4, :l, :m)
+    e5 = relabel_input(e5, :l, :k)
+    e6 = relabel_input(e6, :m, :k)
+    query = Query(:out, Materialize(Aggregate(+, :i, :j, :k, :l, :m, MapJoin(*, e1, e2, e3, e4, e5, e6))))
+    return query
 end
 
 time_dict = Dict("balanced triangle"=>Dict(),
@@ -78,23 +48,20 @@ for ST in [DCStats, NaiveStats]
     main_edge = edges[0]
 
     qt_balanced = query_triangle(main_edge, main_edge, main_edge)
-    galley(qt_balanced, faq_optimizer=greedy, verbose=0)
-    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, verbose=verbosity)
-    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, verbose=verbosity)
+    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, ST=ST, verbose=verbosity)
+    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, ST=ST, verbose=verbosity)
     println("Balanced Triangle [$ST]: ", qt_balanced_time)
     time_dict["balanced triangle"][string(ST)] = qt_balanced_time
 
     qp_balanced = query_path(main_edge, main_edge, main_edge, main_edge)
-    galley(qp_balanced, faq_optimizer=greedy, verbose=0)
-    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, verbose=verbosity)
-    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, verbose=verbosity)
+    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, ST=ST, verbose=verbosity)
+    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, ST=ST, verbose=verbosity)
     println("Balanced Path [$ST]: ", qp_balanced_time)
     time_dict["balanced path"][string(ST)] = qp_balanced_time
 
     qb_balanced = query_bowtie(main_edge, main_edge, main_edge, main_edge, main_edge, main_edge)
-    galley(qb_balanced, faq_optimizer=greedy, verbose=0)
-    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, verbose=verbosity)
-    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, verbose=verbosity)
+    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, ST=ST, verbose=verbosity)
+    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, ST=ST, verbose=verbosity)
     println("Balanced Bowtie [$ST]: ", qb_balanced_time)
     time_dict["balanced bowtie"][string(ST)] = qb_balanced_time
 end
@@ -105,20 +72,20 @@ for ST in [DCStats, NaiveStats]
     main_edge = edges[0]
 
     qt_balanced = query_triangle(main_edge, main_edge, main_edge)
-    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, dbconn=dbconn, verbose=verbosity)
-    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, dbconn=dbconn, verbose=verbosity)
+    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, ST=ST, dbconn=dbconn, verbose=verbosity)
+    qt_balanced_time = galley(qt_balanced, faq_optimizer=greedy, ST=ST, dbconn=dbconn, verbose=verbosity)
     println("Balanced Triangle [$ST]: ", qt_balanced_time)
     time_dict["balanced triangle"][string(ST) * "_duckdb"] = qt_balanced_time
 
     qp_balanced = query_path(main_edge, main_edge, main_edge, main_edge)
-    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, dbconn=dbconn, verbose=verbosity)
-    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, dbconn=dbconn, verbose=verbosity)
+    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, ST=ST, dbconn=dbconn, verbose=verbosity)
+    qp_balanced_time = galley(qp_balanced, faq_optimizer=greedy, ST=ST, dbconn=dbconn, verbose=verbosity)
     println("Balanced Path [$ST]: ", qp_balanced_time)
     time_dict["balanced path"][string(ST) * "_duckdb"] = qp_balanced_time
 
     qb_balanced = query_bowtie(main_edge, main_edge, main_edge, main_edge, main_edge, main_edge)
-    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, dbconn=dbconn, verbose=verbosity)
-    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, dbconn=dbconn, verbose=verbosity)
+    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, ST=ST, dbconn=dbconn, verbose=verbosity)
+    qb_balanced_time = galley(qb_balanced, faq_optimizer=greedy, ST=ST, dbconn=dbconn, verbose=verbosity)
     println("Balanced Bowtie [$ST]: ", qb_balanced_time)
     time_dict["balanced bowtie"][string(ST) * "_duckdb"] = qb_balanced_time
 end
@@ -128,20 +95,20 @@ vertices, edges = load_dataset("Experiments/Data/Subgraph_Data/yeast/yeast.graph
 main_edge = edges[0]
 
 qt_balanced = query_triangle(main_edge, main_edge, main_edge)
-qt_balanced_time = galley(qt_balanced, faq_optimizer=naive, dbconn=dbconn, verbose=verbosity)
-qt_balanced_time = galley(qt_balanced, faq_optimizer=naive, dbconn=dbconn, verbose=verbosity)
+qt_balanced_time = galley(qt_balanced, faq_optimizer=naive, ST=NaiveStats, dbconn=dbconn, verbose=verbosity)
+qt_balanced_time = galley(qt_balanced, faq_optimizer=naive, ST=NaiveStats, dbconn=dbconn, verbose=verbosity)
 println("Balanced Triangle [DuckDB]: ", qt_balanced_time)
 time_dict["balanced triangle"]["DuckDB"] = qt_balanced_time
 
 qp_balanced = query_path(main_edge, main_edge, main_edge, main_edge)
-qp_balanced_time = galley(qp_balanced, faq_optimizer=naive, dbconn=dbconn, verbose=verbosity)
-qp_balanced_time = galley(qp_balanced, faq_optimizer=naive, dbconn=dbconn, verbose=verbosity)
+qp_balanced_time = galley(qp_balanced, faq_optimizer=naive, ST=NaiveStats, dbconn=dbconn, verbose=verbosity)
+qp_balanced_time = galley(qp_balanced, faq_optimizer=naive, ST=NaiveStats, dbconn=dbconn, verbose=verbosity)
 println("Balanced Path [DuckDB]: ", qp_balanced_time)
 time_dict["balanced path"]["DuckDB"] = qp_balanced_time
 
 qb_balanced = query_bowtie(main_edge, main_edge, main_edge, main_edge, main_edge, main_edge)
-qb_balanced_time = galley(qb_balanced, faq_optimizer=naive, dbconn=dbconn, verbose=verbosity)
-qb_balanced_time = galley(qb_balanced, faq_optimizer=naive, dbconn=dbconn, verbose=verbosity)
+qb_balanced_time = galley(qb_balanced, faq_optimizer=naive, ST=NaiveStats, dbconn=dbconn, verbose=verbosity)
+qb_balanced_time = galley(qb_balanced, faq_optimizer=naive, ST=NaiveStats, dbconn=dbconn, verbose=verbosity)
 println("Balanced Bowtie [DuckDB]: ", qb_balanced_time)
 time_dict["balanced bowtie"]["DuckDB"] = qb_balanced_time
 
@@ -172,7 +139,7 @@ gbplot = StatsPlots.groupedbar(Xs,
                                 execute_times,
                                 group = groups,
                                 yscale =:log10,
-                                ylims=[10^-3, 10^2],
+                                ylims=[10^-4, 10^2],
                                 legend = :outertopleft,
                                 size = (1400, 600))
 xlabel!(gbplot, "Query Type")

1a789345324cc51e0a0ac4820192bd171e73a54b
kylebd99 <kdeeds@cs.washington.edu>
2024-05-20 11:17:59 -0700

    add alu example

---
+++ Experiments/Scripts/alu_example.jl
@@ -0,0 +1,38 @@
+using Finch
+using Galley
+
+vertex_vectors, edge_matrices = load_subgraph_dataset(wordnet, DCStats, nothing)
+X = Tensor(Dense(SparseList(Element(0))), edge_matrices[0].tns.val)
+n = size(X)[1]
+#n = 100000
+#A = Tensor(Dense(SparseList(Pattern())), fsprand(Bool, n, n, 20 * (1.0/n)))
+u = Tensor(Dense(Element(0)), rand(Int, n) .% 100)
+v = Tensor(Dense(Element(0)), rand(Int, n) .% 100)
+l = Scalar(0)
+
+f_time = @elapsed @finch begin
+    l.=0
+    for i =_
+        for j =_
+            l[] += (X[j,i] - u[i]*v[j])^2
+        end
+    end
+end
+f_time = @elapsed @finch begin
+    l.=0
+    for i =_
+        for j =_
+            l[] += (X[j,i] - u[i]*v[j])^2
+        end
+    end
+end
+
+q = Materialize(Aggregate(+, :i, :j, MapJoin(*, MapJoin(+, Input(X, :j, :i), MapJoin(*, MapJoin(-, Input(u, :i)), Input(v, :j))),
+                                                MapJoin(+, Input(X, :j, :i), MapJoin(*, MapJoin(-, Input(u, :i)), Input(v, :j))))))
+
+result = galley(deepcopy(Query(:out, q)), ST=DCStats, verbose=3)
+result_galley = galley(deepcopy(Query(:out, q)), ST=DCStats, verbose=0)
+println("Galley Exec: $(result_galley.execute_time)")
+println("Galley Opt: $(result_galley.opt_time)")
+println("Finch Exec: $(f_time)")
+println("F = G: $(l[] - result_galley.value))")
--- Experiments/Scripts/pagerank_example.jl
+++ Experiments/Scripts/pagerank_example.jl
@@ -3,7 +3,7 @@ using Finch
 using Galley
 using Galley: t_dense, t_sparse_list
 
-vertex_vectors, edge_matrices = load_subgraph_dataset(youtube, DCStats, nothing)
+vertex_vectors, edge_matrices = load_subgraph_dataset(aids, DCStats, nothing)
 edges = Tensor(Dense(SparseList(Element(0))), edge_matrices[0].tns.val)
 #edges = Tensor(Dense(SparseList(Element(0.0))), fsprand(Bool, 100, 100, .01))
 n = size(edges)[1]
@@ -32,9 +32,13 @@ rank_1_g = Materialize(t_dense, :i, Aggregate(+, :j, MapJoin(*, Input(scaled_edg
 r_1_g = Materialize(t_dense, :i, MapJoin(+, beta_score, MapJoin(*, Input(rank_1_g, :i), damp)))
 rank_2_g = Materialize(t_dense, :i, Aggregate(+, :j, MapJoin(*, Input(scaled_edges, :i, :j), Input(r_1_g, :j))))
 r_2_g = Materialize(t_dense, :i, MapJoin(+, beta_score, MapJoin(*, Input(rank_2_g, :i), damp)))
+rank_3_g = Materialize(t_dense, :i, Aggregate(+, :j, MapJoin(*, Input(scaled_edges, :i, :j), Input(r_2_g, :j))))
+r_3_g = Materialize(t_dense, :i, MapJoin(+, beta_score, MapJoin(*, Input(rank_3_g, :i), damp)))
+rank_4_g = Materialize(t_dense, :i, Aggregate(+, :j, MapJoin(*, Input(scaled_edges, :i, :j), Input(r_3_g, :j))))
+r_4_g = Materialize(t_dense, :i, MapJoin(+, beta_score, MapJoin(*, Input(rank_4_g, :i), damp)))
 
-result_galley = galley(deepcopy(Query(:out, r_2_g)), ST=DCStats, verbose=3)
-result_galley = galley(deepcopy(Query(:out, r_2_g)), ST=DCStats, verbose=0)
+result_galley = galley(Query(:out, r_4_g), ST=DCStats, verbose=3)
+result_galley = galley(Query(:out, r_4_g), ST=DCStats, verbose=0)
 println("Galley Exec: $(result_galley.execute_time)")
 println("Galley Opt: $(result_galley.opt_time)")
 
@@ -44,8 +48,17 @@ f_time = @elapsed begin
     @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
     @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
     @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
+    @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
+    @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
+    @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
+    @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
 end
+
 f_time = @elapsed begin
+    @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
+    @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
+    @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
+    @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
     @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
     @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
     @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
@@ -54,35 +67,3 @@ end
 
 println("Finch Exec: $(f_time)")
 println("F = G: $(all(abs.(r .- result_galley.value) .< .01))")
-
-"""
-    pagerank(adj; [nsteps], [damp])
-
-Calculate `nsteps` steps of the page rank algorithm on the graph specified by
-the adjacency matrix `adj`. `damp` is the damping factor.
-"""
-function pagerank(edges; nsteps=20, damp = 0.85)
-    (n, m) = size(edges)
-    @assert n == m
-    out_degree = Tensor(Dense(Element(0)))
-    @finch (out_degree .= 0; for j=_, i=_; out_degree[j] += edges[i, j] end)
-    scaled_edges = Tensor(Dense(SparseList(Element(0.0))))
-    @finch begin
-        scaled_edges .= 0
-        for j = _, i = _
-            if out_degree[i] != 0
-                scaled_edges[i, j] = edges[i, j] / out_degree[j]
-            end
-        end
-    end
-    r = Tensor(Dense(Element(0.0)), n)
-    @finch (r .= 0.0; for j=_; r[j] = 1.0/n end)
-    rank = Tensor(Dense(Element(0.0)), n)
-    beta_score = (1 - damp)/n
-
-    for step = 1:nsteps
-        @finch (rank .= 0; for j=_, i=_; rank[i] += scaled_edges[i, j] * r[j] end)
-        @finch (r .= 0.0; for i=_; r[i] = beta_score + damp * rank[i] end)
-    end
-    return r
-end

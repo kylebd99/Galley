7de9b99c100b8a4dc001cbd6e90b8281ce95228b
kylebd99 <kdeeds@cs.washington.edu>
2024-05-31 11:31:19 -0700

    fix pruned optimizer

--- src/FAQOptimizer/pruned-optimizer.jl
+++ src/FAQOptimizer/pruned-optimizer.jl
@@ -1,22 +1,29 @@
 
 
 
-function beam_search(input_query::PlanNode, ST, k, max_cost)
+function branch_and_bound(input_query::PlanNode, ST, k, max_cost)
     input_aq = AnnotatedQuery(input_query, ST)
     PLAN_AND_COST = Tuple{Vector{PlanNode}, Vector{PlanNode}, AnnotatedQuery, Float64}
     optimal_orders = Dict{Set{IndexExpr}, PLAN_AND_COST}(Set{IndexExpr}()=>(PlanNode[], PlanNode[], input_aq, 0))
-    for min_reduced_idxs in 1:length(input_aq.reduce_idxs)
+    prev_new_optimal_orders = optimal_orders
+    # To speed up inference, we cache cost calculations for each set of already reduced idxs
+    # and proposed reduction index.
+    cost_cache = Dict()
+    for _ in 1:length(input_aq.reduce_idxs)
         new_optimal_orders = Dict{Set{IndexExpr}, PLAN_AND_COST}()
-        for (vars, pc) in optimal_orders
+        for (vars, pc) in prev_new_optimal_orders
             aq = pc[3]
             prev_cost = pc[4]
             for idx in get_reducible_idxs(aq)
-                cost, reduced_vars = cost_of_reduce(idx, aq)
+                if !haskey(cost_cache, (vars, idx))
+                    cost_cache[(vars, idx)] = cost_of_reduce(idx, aq)
+                end
+                cost, reduced_vars = cost_cache[(vars, idx)]
                 cost += prev_cost
                 new_vars = union(vars, [i.name for i in reduced_vars])
                 cheapest_cost = min(get(new_optimal_orders, new_vars, (nothing, nothing, nothing, Inf))[4],
                                     get(optimal_orders, new_vars, (nothing, nothing, nothing, Inf))[4])
-                if cost <= min(max_cost, cheapest_cost)
+                if cost <= min(max_cost, cheapest_cost) + 1 # We add 1 to avoid FP issues
                     new_order = [pc[1]..., idx]
                     new_aq = copy(aq)
                     query = reduce_idx!(idx, new_aq)
@@ -25,19 +32,21 @@ function beam_search(input_query::PlanNode, ST, k, max_cost)
                 end
             end
         end
-        filter!((x)->length(x[1]) >= min_reduced_idxs, optimal_orders)
-        for i in min_reduced_idxs:length(input_aq.reduce_idxs)
-            length_i_opt_orders = [x for x in new_optimal_orders if length(x[1]) == i]
-            num_orders = Int(min(k, length(length_i_opt_orders)))
-            merge!(optimal_orders, Dict(sort(length_i_opt_orders, by=(v_and_pc)->v_and_pc[2][4])[1:num_orders]))
+        merge!(optimal_orders, new_optimal_orders)
+        # At each step, we only keep the k cheapest plans for each # of reduced idxs
+        prev_new_optimal_orders = Dict()
+        for i in 1:length(input_aq.reduce_idxs)
+            i_length = filter((v_p)->length(v_p[1])==i, new_optimal_orders)
+            num_to_keep = Int(min(k, length(i_length)))
+            merge!(prev_new_optimal_orders, Dict(sort(collect(i_length), by=(v_p)->v_p[2][4])[1:num_to_keep]))
         end
     end
-    return only(values(optimal_orders))
+    return optimal_orders[Set([i.name for i in input_aq.reduce_idxs])]
 end
 
 function pruned_query_to_plan(input_query::PlanNode, ST)
-    greedy_order, greedy_queries, greedy_aq, greedy_cost = beam_search(plan_copy(input_query), ST, 3, Inf)
-    exact_order, exact_queries, exact_aq, exact_cost = beam_search(plan_copy(input_query), ST, Inf, greedy_cost)
+    greedy_order, greedy_queries, greedy_aq, greedy_cost = branch_and_bound(plan_copy(input_query), ST, 1, Inf)
+    exact_order, exact_queries, exact_aq, exact_cost = branch_and_bound(plan_copy(input_query), ST, Inf, greedy_cost)
     remaining_q = get_remaining_query(exact_aq)
     if !isnothing(remaining_q)
         push!(exact_queries, remaining_q)

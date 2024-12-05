554a3d8eeb9c25f9c232e7b14771727a1ae677f9
kylebd99 <kdeeds@cs.washington.edu>
2024-12-05 14:02:59 -0800

    add elementwise ops

--- Experiments/Scripts/matrix_chain_galley.jl
+++ Experiments/Scripts/matrix_chain_galley.jl
@@ -7,6 +7,7 @@ function main()
     forward_times = []
     backward_times =[]
     sum_times = []
+    elementwise_times = []
     for d in densities
         A = Tensor(Dense(SparseList(Element(0.0))), fsprand(N, N, .5))
         B = Tensor(Dense(SparseList(Element(0.0))), fsprand(N, N, .5))
@@ -15,12 +16,14 @@ function main()
         avg_time_forward = 0
         avg_time_backward = 0
         avg_time_sum = 0
+        avg_time_elementwise = 0
         for i in 1:n_reps
             verbosity = i == 2 ? 3 : 0
             if i == 3
                 avg_time_forward = 0
                 avg_time_backward = 0
                 avg_time_sum = 0
+                avg_time_elementwise = 0
             end
 
             E1 = Query(:E1, Mat(:i, :l, Σ(:j, :k, MapJoin(*, Input(A, :i, :j), Input(B, :j, :k), Input(C, :k, :l)))))
@@ -40,21 +43,68 @@ function main()
             galley(E3, verbose=verbosity, faq_optimizer=greedy)
             end_time = time()
             avg_time_sum += end_time - start_time
+
+            A = Tensor(Dense(Dense(Element(0.0))), rand(N, N))
+            B = Tensor(Dense(Dense(Element(0.0))), rand(N, N))
+            C = Tensor(Dense(SparseList(Element(0.0))), fsprand(N, N, d))
+
+            E4 = Query(:E4,  Mat(:i, :j, MapJoin(*, Input(A, :i, :j), Input(B, :i, :j), Input(C, :i, :j))))
+            start_time = time()
+            galley(E4, verbose=verbosity, faq_optimizer=greedy)
+            end_time = time()
+            avg_time_elementwise += end_time - start_time
         end
         avg_time_forward /= n_reps - 2
         avg_time_backward /= n_reps - 2
         avg_time_sum /= n_reps - 2
+        avg_time_elementwise /= n_reps - 2
         push!(forward_times, avg_time_forward)
         push!(backward_times, avg_time_backward)
         push!(sum_times, avg_time_sum)
+        push!(elementwise_times, avg_time_elementwise)
         println("Density: ", d)
         println("Forward Time: ", avg_time_forward)
         println("Backward Time: ", avg_time_backward)
         println("Sum Time: ", avg_time_sum)
+        println("Elementwise Time: ", avg_time_elementwise)
     end
     println(forward_times)
     println(backward_times)
     println(sum_times)
+    println(elementwise_times)
 end
 
 main()
+
+
+using Plots
+using StatsPlots
+using DataFrames
+using CSV
+
+data = DataFrame(CSV.File("Experiments/Results/mat_chain.csv"))
+
+abc_plt = @df data[data.Algorithm .=="ABC", :] bar([i%6 + floor(i/6)/3 for i in 0:17], :Runtime, group=:Method,
+                                        xticks=([.33, 1.33, 2.33, 3.33, 4.33, 5.33], [".1", ".01", ".001", ".0001", ".00001", ".000001"]),
+                                        yscale=:log10, fillrange = 10^(-2), ylims=[10^(-2), 100],
+                                        xflip=false, lw=2, xtickfontsize=12,  bar_width=.25,
+                                        ytickfontsize=12, legendfontsize=12, legend=:topright,
+                                        xlabel="Density of C", ylabel="Runtime", title="Matrix Chain Multiplication (ABC)")
+savefig(abc_plt, "Experiments/Figures/mat_chain_abc.png")
+
+
+cba_plt = @df data[data.Algorithm .=="CBA", :] bar([i%6 + floor(i/6)/3 for i in 0:17], :Runtime, group=:Method,
+                                        xticks=([.33, 1.33, 2.33, 3.33, 4.33, 5.33], [".1", ".01", ".001", ".0001", ".00001", ".000001"]),
+                                        yscale=:log10, fillrange = 10^(-2), ylims=[10^(-2), 100],
+                                        xflip=false, lw=2, xtickfontsize=12,  bar_width=.25,
+                                        ytickfontsize=12, legendfontsize=12, legend=:topright,
+                                        xlabel="Density of C", ylabel="Runtime", title="Matrix Chain Multiplication (CBA)")
+savefig(cba_plt, "Experiments/Figures/mat_chain_cba.png")
+
+sum_plt = @df data[data.Algorithm .=="SUM(ABC)", :] bar([i%6 + floor(i/6)/3 for i in 0:17], :Runtime, group=:Method,
+                                        xticks=([.33, 1.33, 2.33, 3.33, 4.33, 5.33], [".1", ".01", ".001", ".0001", ".00001", ".000001"]),
+                                        yscale=:log10, fillrange = 10^(-2), ylims=[10^(-2), 100],
+                                        xflip=false, lw=2, xtickfontsize=12,  bar_width=.25,
+                                        ytickfontsize=12, legendfontsize=12, legend=:topright,
+                                        xlabel="Density of C", ylabel="Runtime", title="Matrix Chain Multiplication (SUM(ABC))")
+savefig(sum_plt, "Experiments/Figures/mat_chain_sum.png")
--- Experiments/Scripts/matrix_chain_tensorflow.py
+++ Experiments/Scripts/matrix_chain_tensorflow.py
@@ -9,28 +9,37 @@ def mat_chain(A, B, C):
 def mat_chain_back(A, B, C):
     return C @ B @ A
 
+def mat_elementwise(A, B, C):
+    return A * B * C
+
 N = 2000
 densities = [.1, .01, .001, .0001, .00001, .000001]
 forward_times = []
 backward_times =[]
 sum_times = []
+elementwise_times = []
 for d in densities:
     A = torch.from_numpy(np.astype(np.random.rand(N,N) < .5, float)).to_sparse_csc()
     B = torch.from_numpy(np.astype(np.random.rand(N,N) < .5, float)).to_sparse_csc()
+    A_dense = torch.from_numpy(np.astype(np.random.rand(N,N), float))
+    B_dense = torch.from_numpy(np.astype(np.random.rand(N,N), float))
     C = torch.from_numpy(np.astype(np.random.rand(N,N) < d, float)).to_sparse_csc()
 
     scripted_mat_chain = torch.jit.script(mat_chain, example_inputs=[(A, B, C)])
     scripted_mat_chain_back = torch.jit.script(mat_chain_back, example_inputs=[(A, B, C)])
+    scripted_mat_elementwise = torch.jit.script(mat_chain_back, example_inputs=[(A_dense, B_dense, C)])
 
     n_reps = 7
     avg_time_forward = 0
     avg_time_backward = 0
     avg_time_sum = 0
+    avg_time_elementwise = 0
     for i in range(1, n_reps+1):
         if i < 3:
             avg_time_forward = 0
             avg_time_backward = 0
             avg_time_sum = 0
+            avg_time_elementwise = 0
         start = time.time()
         E1 = scripted_mat_chain(A, B, C)
         end = time.time()
@@ -46,16 +55,25 @@ for d in densities:
         end = time.time()
         avg_time_sum = avg_time_sum + end-start
 
+        start = time.time()
+        E3 = scripted_mat_elementwise(A_dense, B_dense, C)
+        end = time.time()
+        avg_time_elementwise = avg_time_elementwise + end-start
+
     avg_time_forward = avg_time_forward / (n_reps - 2) 
     avg_time_backward = avg_time_backward / (n_reps - 2) 
     avg_time_sum = avg_time_sum / (n_reps - 2) 
+    avg_time_elementwise = avg_time_elementwise / (n_reps - 2) 
     forward_times.append(avg_time_forward)
     backward_times.append(avg_time_backward)
     sum_times.append(avg_time_sum)
+    elementwise_times.append(avg_time_elementwise)
     print("Density: ", d)
     print("Forward Time: ", avg_time_forward)
     print("Backward Time: ", avg_time_backward)
     print("Sum Time: ", avg_time_sum)
+    print("Elementwise Time: ", avg_time_elementwise)
 print(forward_times)
 print(backward_times)
 print(sum_times)
+print(elementwise_times)
